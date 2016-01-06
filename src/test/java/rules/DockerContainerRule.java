5a0aad04a85332c67ff5f11c52b47ded6caf31d9
geowarin <geowarin@gmail.com>
2016-01-06 14:35:17 +0100

    Unix support

--- src/test/java/rules/DockerContainerRule.java
+++ src/test/java/rules/DockerContainerRule.java
@@ -2,6 +2,8 @@ package rules;
 
 import com.spotify.docker.client.*;
 import com.spotify.docker.client.messages.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.rules.ExternalResource;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -17,7 +19,8 @@ import java.util.Map;
  * Adapted from https://gist.github.com/mosheeshel/c427b43c36b256731a0b
  */
 public class DockerContainerRule extends ExternalResource {
-    public static final String DOCKER_SERVICE_URL = "http://192.168.99.100:2375";
+    protected final Log logger = LogFactory.getLog(getClass());
+    public static final String DOCKER_MACHINE_SERVICE_URL = "https://192.168.99.100:2376";
 
     private final DockerClient dockerClient;
     private ContainerCreation container;
@@ -32,19 +35,7 @@ public class DockerContainerRule extends ExternalResource {
     }
 
     public DockerContainerRule(String imageName, String[] ports, String cmd) {
-
-//        dockerClient = new DefaultDockerClient(dockerServiceUrl);
-        DockerCertificates dockerCertificates;
-        try {
-            String userHome = System.getProperty("user.home");
-            dockerCertificates = new DockerCertificates(Paths.get(userHome, ".docker/machine/certs"));
-        } catch (DockerCertificateException e) {
-            throw new IllegalStateException(e);
-        }
-        dockerClient = DefaultDockerClient.builder()
-                .uri(URI.create("https://192.168.99.100:2376"))
-                .dockerCertificates(dockerCertificates)
-                .build();
+        dockerClient = createDockerClient();
 
         Map<String, List<PortBinding>> portBindings = new HashMap<>();
         for (String port : ports) {
@@ -76,6 +67,29 @@ public class DockerContainerRule extends ExternalResource {
         }
     }
 
+    private DockerClient createDockerClient() {
+        if (isUnix() || System.getenv("DOCKER_HOST") != null) {
+            try {
+                return DefaultDockerClient.fromEnv().build();
+            } catch (DockerCertificateException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
+        logger.info("Could not create docker client from the environment. Assuming docker-machine environment");
+        DockerCertificates dockerCertificates = null;
+        try {
+            String userHome = System.getProperty("user.home");
+            dockerCertificates = new DockerCertificates(Paths.get(userHome, ".docker/machine/certs"));
+        } catch (DockerCertificateException e) {
+            System.err.println(e.getMessage());
+        }
+        return DefaultDockerClient.builder()
+                .uri(URI.create(DOCKER_MACHINE_SERVICE_URL))
+                .dockerCertificates(dockerCertificates)
+                .build();
+    }
+
     @Override
     public Statement apply(Statement base, Description description) {
         return super.apply(base, description);
@@ -95,6 +109,11 @@ public class DockerContainerRule extends ExternalResource {
             return -1;
         }
         return Integer.parseInt(portBindings.get(0).hostPort());
+    }
+
+    private static boolean isUnix() {
+        String os = System.getProperty("os.name").toLowerCase();
+        return os.contains("nix") || os.contains("nux") || os.contains("aix");
     }
 
     public String getDockerHost() {
